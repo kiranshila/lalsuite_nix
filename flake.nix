7ccb02d4af9286f0228913049f913bfe6b007e48
Kiran Shila <me@kiranshila.com>
2024-07-06 18:53:58 -0700

    Reorganize

--- flake.nix
+++ flake.nix
@@ -1,33 +1,41 @@
 {
-  inputs.nixpkgs.url = "github:kiranshila/nixpkgs/master";
-  outputs = {
-    self,
-    nixpkgs,
-  }: {
-    defaultPackage.x86_64-linux = with import nixpkgs {system = "x86_64-linux";};
-      clangStdenv.mkDerivation {
-        name = "lalsuite";
-
-        src = fetchurl {
-          url = "https://software.igwn.org/lscsoft/source/lalsuite/lal-7.5.0.tar.xz";
-          hash = "sha256-m7aSATxTSIDPPut8jkXCoyvW10588MaRj2OLwVvQz10=";
-        };
-
-        nativeBuildInputs = with pkgs; [autoconf automake pkg-config git];
-
-        buildInputs = with pkgs; [
-          zlib
-          gsl
-          fftw
-          fftwFloat
-          (pkgs.python3.withPackages (pp: [
-            pp.numpy
-          ]))
-        ];
-
-        configurePhase = ''
-          ./configure --disable-swig --prefix=$out CFLAGS="-Wno-macro-redefined -flto" LDFLAGS="-flto"
-        '';
-      };
+  description = "LALSuite in a nix flake";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (system: let
+        # Setup nixpkgs
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        # Build LALSuite
+        lalsuiteDeriv = with pkgs;
+          clangStdenv.mkDerivation rec {
+            pname = "lalsuite";
+            version = "7.5.0";
+            src = fetchurl {
+              url = "https://software.igwn.org/lscsoft/source/lalsuite/lal-${version}.tar.xz";
+              hash = "sha256-m7aSATxTSIDPPut8jkXCoyvW10588MaRj2OLwVvQz10=";
+            };
+            nativeBuildInputs = with pkgs; [autoconf automake pkg-config git];
+            buildInputs = with pkgs; [
+              zlib
+              gsl
+              fftw
+              fftwFloat
+            ];
+            configurePhase = ''
+              ./configure --disable-swig --prefix=$out CFLAGS="-Wno-macro-redefined -flto" LDFLAGS="-flto"
+            '';
+          };
+      in {
+        packages = {
+          default = lalsuiteDeriv;
+          lalsuite = lalsuiteDeriv;
+        };
+      });
 }
